a47dc166808cf039854480046ff2d2379654faa3
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-06 19:52:52 -0400

    Deploy

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -9,7 +9,7 @@ exec {'Setup_server':
     echo \'Server configured\' > /data/web_static/releases/test/index.html
     ln -s /data/web_static/releases/test/ /data/web_static/current
     chown -R ubuntu:ubuntu /data/
-    sudo sed -i \'/server_name _;/a \\tlocation /hbnb_static
-    {\n\t\talias /data/web_static/current/;\n\t\ttry_files $uri $uri/ =404;\n\t}\' /etc/nginx/sites-enabled/default
+    sudo sed -i\
+    \'/server_name _;/a \\\tlocation \hbnb_static {\n\t\talias /data/web_static/current/;\n\t\ttry_files $uri $uri/ =404;\n\t}\' /etc/nginx/sites-enabled/default
     sudo service nginx restart'
 }
