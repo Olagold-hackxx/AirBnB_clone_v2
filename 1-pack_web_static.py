4bcd7a204dda5f4b7865419a2ff7dd5b936244b8
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-06 15:47:50 -0400

    Debug

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,9 +15,9 @@ def do_pack():
                 time.second)
     print("Packing web_static to {}".format(path))
     try:
-        result = local("tar -cvzf {} web_static".format(path))
+        local("tar -cvzf {} web_static".format(path))
         size = os.stat(path).st_size
-        print("web_static packed: {} -> {} Bytes".format(path, size))
+        print("web_static packed: {} -> {}Bytes".format(path, size))
     except:
         return None
     return path
