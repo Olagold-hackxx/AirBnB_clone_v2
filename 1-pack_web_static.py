7d032446da9658685fc2d0c3de596baed5b2974f
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-06 12:59:46 -0400

    Setup server

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,11 @@ def do_pack():
     """Generates a .tgz archive"""
     local("mkdir -p versions")
     time = datetime.now()
-    path = "versions/web_static_{}{}{}{}{}{}".format(time.year,
+    path = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
                                                      time.month, time.day,
                                                      time.hour, time.minute,
                                                      time.second)
-    return_value = local("tar -cvzf {}.tgz web_static".format(path))
-    if return_value != 0:
+    result = local("tar -cvzf {} web_static".format(path))
+    if result.failed:
         return None
     return path
