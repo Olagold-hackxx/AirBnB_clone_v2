3dbeccc8ab0a1c6ce71af07ed0814a7a23e5d9af
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-06 13:03:28 -0400

    Setup server

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,14 +3,16 @@
 from fabric.api import *
 from datetime import datetime
 
+
 def do_pack():
     """Generates a .tgz archive"""
     local("mkdir -p versions")
     time = datetime.now()
     path = "versions/web_static_{}{}{}{}{}{}.tgz".format(time.year,
-                                                     time.month, time.day,
-                                                     time.hour, time.minute,
-                                                     time.second)
+                                                         time.month, time.day,
+                                                         time.hour,
+                                                         time.minute,
+                                                         time.second)
     result = local("tar -cvzf {} web_static".format(path))
     if result.failed:
         return None
