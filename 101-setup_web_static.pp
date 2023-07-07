1a184209ec3959d58c43fdea53f3f73f0f284943
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-06 21:03:19 -0400

    Deploy

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,16 +1,16 @@
 # Set server
 exec {'Setup_server':
     provider => 'shell',
-    command  => 'sudo apt-get -y update
-    sudo apt-get -y install nginx
-    sudo service nginx start
-    sudo mkdir -p /data/web_static/shared/
-    sudo mkdir -p /data/web_static/releases/test/
-    echo \'Server configured\' > sudo /data/web_static/releases/test/index.html
-    sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-    chown -R ubuntu:ubuntu /data/
+    command  => 'sudo apt-get -y update;
+    sudo apt-get -y install nginx;
+    sudo service nginx start;
+    sudo mkdir -p /data/web_static/shared/;
+    sudo mkdir -p /data/web_static/releases/test/;
+    echo \'Server configured\' > sudo /data/web_static/releases/test/index.html;
+    sudo ln -s /data/web_static/releases/test/ /data/web_static/current;
+    chown -R ubuntu:ubuntu /data/;
     sudo sed -i\
     \'/server_name _;/a \\\tlocation \hbnb_static {\n\t\talias /data/web_static/current/;\n\t\ttry_files $uri $uri/ =404;\n\t}\'\
-    /etc/nginx/sites-enabled/default
+    /etc/nginx/sites-enabled/default;
     sudo service nginx restart'
 }
