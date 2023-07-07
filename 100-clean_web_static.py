c2621a58385e6d33cd37dbe89aaa7b91ef33dc6f
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-06 20:58:49 -0400

    Deploy

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from fabric.api import *
 import os
-"""Generates a tar archive"""
+"""Clean up servers"""
 
 env.hosts = ['18.233.67.176', '35.175.135.215']
 env.user = 'ubuntu'
 
 
 def do_clean(number=0):
+    """Delete old versions of archives"""
     local_archives = sorted(os.listdir("versions"))
     remote_archives = run("ls /data/web_static/releases/*web_static*").split()
     if number == 0:
