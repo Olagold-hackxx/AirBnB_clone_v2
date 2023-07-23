c33950a49d94abeee9d1867d13e4d46041013c58
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-23 18:37:06 -0400

    Fix checkers errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
-"""Start a flak web app
-"""
-from flask import Flask, render_template
-from models import storage
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.strict_slashes = False
-data = storage.all('State').values()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+
+    States are sorted by name.
+    """
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def session_close(session=None):
-    """Close db session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list')
-def state_list():
-    """Get state list"""
-    return render_template('7-states_list.html', states=data)
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
